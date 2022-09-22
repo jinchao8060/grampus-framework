7aab0108bbd5dccb6ebffaaea8c77076543b5c84
Beck <297513931@qq.com>
2022-09-22 18:33:06 +0800

    :bug: knife4j改为使用springfox-swagger3.0.0版本，添加springfox与springboot2.6.x版本兼容性问题修复代码.

--- grampus-framework-swagger/grampus-framework-knife4j/src/main/java/com/oceancloud/grampus/framework/swagger/SwaggerAutoConfiguration.java
+++ grampus-framework-swagger/grampus-framework-knife4j/src/main/java/com/oceancloud/grampus/framework/swagger/SwaggerAutoConfiguration.java
@@ -2,20 +2,25 @@ package com.oceancloud.grampus.framework.swagger;
 
 import com.github.xiaoymin.knife4j.spring.annotations.EnableKnife4j;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.ReflectionUtils;
+import org.springframework.web.servlet.mvc.method.RequestMappingInfoHandlerMapping;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.ApiKey;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
-//import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import springfox.documentation.spring.web.plugins.WebMvcRequestHandlerProvider;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-import java.util.Collections;
+import java.lang.reflect.Field;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Swagger配置
@@ -24,8 +29,7 @@ import java.util.Collections;
  * @since 2020-4-7
  */
 @Configuration
-//@EnableSwagger2
-@EnableSwagger2WebMvc
+@EnableSwagger2
 @EnableKnife4j
 public class SwaggerAutoConfiguration {
 
@@ -52,5 +56,47 @@ public class SwaggerAutoConfiguration {
 				.version("1.0.0")
 				.contact(new Contact("Beck", "", ""))
 				.build();
+	}
+
+	/**
+	 * Spring Boot 2.6.x版本引入依赖 springfox-boot-starter (Swagger 3.0) 后，启动容器会报错：
+	 * <p>
+	 * Failed to start bean ‘ documentationPluginsBootstrapper ‘ ; nested exception…
+	 * <p>
+	 * 原因
+	 * Springfox 假设 Spring MVC 的路径匹配策略是 ant-path-matcher，而 Spring Boot 2.6.x版本的默认匹配策略是 path-pattern-matcher，这就造成了上面的报错。
+	 */
+	@Bean
+	public BeanPostProcessor springfoxHandlerProviderBeanPostProcessor() {
+		return new BeanPostProcessor() {
+
+			@Override
+			public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+//				if (bean instanceof WebMvcRequestHandlerProvider || bean instanceof WebFluxRequestHandlerProvider) {
+				if (bean instanceof WebMvcRequestHandlerProvider) {
+					customizeSpringfoxHandlerMappings(getHandlerMappings(bean));
+				}
+				return bean;
+			}
+
+			private <T extends RequestMappingInfoHandlerMapping> void customizeSpringfoxHandlerMappings(List<T> mappings) {
+				List<T> copy = mappings.stream()
+						.filter(mapping -> mapping.getPatternParser() == null)
+						.collect(Collectors.toList());
+				mappings.clear();
+				mappings.addAll(copy);
+			}
+
+			@SuppressWarnings("unchecked")
+			private List<RequestMappingInfoHandlerMapping> getHandlerMappings(Object bean) {
+				try {
+					Field field = ReflectionUtils.findField(bean.getClass(), "handlerMappings");
+					field.setAccessible(true);
+					return (List<RequestMappingInfoHandlerMapping>) field.get(bean);
+				} catch (IllegalArgumentException | IllegalAccessException e) {
+					throw new IllegalStateException(e);
+				}
+			}
+		};
 	}
 }
